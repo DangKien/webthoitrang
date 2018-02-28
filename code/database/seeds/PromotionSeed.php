04d2109d4765bd84648bd439a508a88e6ebbea49
DangKien <dangtrungkien96@gmail.com>
2018-02-28 11:51:24 +0700

    Promotion(Khuyến mãi) Create database seerder

---
+++ code/database/seeds/PromotionSeed.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class PromotionSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('promotions')->insert([
+                    ['name' => 'Giảm giá'],
+                    ['name' => 'Tặng Kèm'],
+                ]);
+    }
+}
