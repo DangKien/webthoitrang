aa26db779bd305adbf5ee96d870432a80285591d
nam_dang <namhythptqo@gmail.com>
2018-03-29 00:02:00 +0700

    handle for statistic product-user-order page

--- code/app/Http/Controllers/BackEnd/StatisticController.php
+++ code/app/Http/Controllers/BackEnd/StatisticController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\BackEnd;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\ProductModel;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StatisticController extends Controller
 {
@@ -14,7 +19,76 @@ class StatisticController extends Controller
      */
     public function index()
     {
-        return view('BackEnd.content.statistic.index');
+        $dt        = Carbon::parse(Carbon::now());
+        $thismonth = $dt->month;
+        $lastmonth = $dt->subMonths(1)->month;
+
+        // for product
+        $product_thismonth       = ProductModel::where(DB::raw('MONTH(created_at)'), '=', $thismonth);
+        $count_product_thismonth = $product_thismonth->count();
+        $product_thismonth       = $product_thismonth->get();
+        $count_product_lastmonth = ProductModel::where(DB::raw('MONTH(created_at)'), '=', $lastmonth)->count();
+        $count_product_all       = ProductModel::count();
+
+        // for new
+        $order_thismonth       = Order::where(DB::raw('MONTH(created_at)'), '=', $thismonth);
+        $count_order_thismonth = $order_thismonth->count();
+        $order_thismonth       = $order_thismonth->get();
+        $order_thismonth       = $order_thismonth->map(function ($item) {
+            $user        = User::find($item->user_id);
+            $item->email = $user->email;
+            if ($item->status == 0) {
+                $item->status = 'Processing';
+            } elseif ($item->status == 1) {
+                $item->status = 'Completed';
+            } elseif ($item->status == 2) {
+                $item->status = 'Pending';
+            } elseif ($item->status == 3) {
+                $item->status = 'Cancel';
+            } elseif ($item->status == 4) {
+                $item->status = 'Refund';
+            } else {
+                $item->status = 'Pending';
+            }
+            return $item;
+        });
+        $count_order_lastmonth = Order::where(DB::raw('MONTH(created_at)'), '=', $lastmonth)->count();
+        $count_order_thisweek  = Order::select('*')
+            ->get()
+            ->groupBy(function ($date) {
+                return Carbon::parse($date->created_at)->format('W');
+            })->count();
+        $count_order_all = Order::count();
+
+        // for user
+        $user_thismonth       = User::where(DB::raw('MONTH(created_at)'), '=', $thismonth);
+        $count_user_thismonth = $user_thismonth->count();
+        $user_thismonth       = $user_thismonth->get();
+        $count_user_lastmonth = User::where(DB::raw('MONTH(created_at)'), '=', $lastmonth)->count();
+        $count_user_thisweek  = User::select('*')
+            ->get()
+            ->groupBy(function ($date) {
+                return Carbon::parse($date->created_at)->format('W');
+            })->count();
+        $count_user_all = User::count();
+
+        $data = [
+            'product_thismonth'       => $product_thismonth,
+            'count_product_thismonth' => $count_product_thismonth,
+            'count_product_lastmonth' => $count_product_lastmonth,
+            'count_product_all'       => $count_product_all,
+            'order_thismonth'         => $order_thismonth,
+            'count_order_thismonth'   => $count_order_thismonth,
+            'count_order_lastmonth'   => $count_order_lastmonth,
+            'count_order_thisweek'    => $count_order_thisweek,
+            'count_order_all'         => $count_order_all,
+            'user_thismonth'          => $user_thismonth,
+            'count_user_thismonth'    => $count_user_thismonth,
+            'count_user_lastmonth'    => $count_user_lastmonth,
+            'count_user_thisweek'     => $count_user_thisweek,
+            'count_user_all'          => $count_user_all,
+        ];
+        return view('BackEnd.content.statistic.index', $data);
     }
 
     /**
--- code/resources/views/BackEnd/content/statistic/index.blade.php
+++ code/resources/views/BackEnd/content/statistic/index.blade.php
@@ -2,16 +2,6 @@
 <div id="content-container">
     <br>
     <div class="col-md-12 col-wrap-user">
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-left">
-                    <h2>Thống kê</h2>
-                </div>
-                <div class="pull-right">
-                    <a class="btn btn-success" href="{{ route('users.create') }}"> Tạo người dùng mới</a>
-                </div>
-            </div>
-        </div>
         @if ($message = Session::get('success'))
         <div class="alert alert-success">
             <p>{{ $message }}</p>
@@ -22,32 +12,284 @@
                 <!--Data Table-->
                 <!--===================================================-->
                 <div class="panel-body">
-                    <div class="tab">
-                        <button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">London</button>
-                        <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
-                        <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
-                    </div>
-                    <div id="London" class="tabcontent">
-                        <h3>London</h3>
-                        <p>London is the capital city of England.</p>
-                    </div>
-                    <div id="Paris" class="tabcontent">
-                        <h3>Paris</h3>
-                        <p>Paris is the capital of France.</p>
-                    </div>
-                    <div id="Tokyo" class="tabcontent">
-                        <h3>Tokyo</h3>
-                        <p>Tokyo is the capital of Japan.</p>
-                    </div>
+			        <h3>Thống kê</h3>
+			        <hr/>
+			        <div class="col-xs-3">
+			            <!-- required for floating -->
+			            <!-- Nav tabs -->
+			            <ul class="nav nav-tabs tabs-left">
+			                <li class="active"><a href="#product" data-toggle="tab">Sản phẩm</a></li>
+			                <li><a href="#order" data-toggle="tab">Đơn hàng</a></li>
+			                <li><a href="#user" data-toggle="tab">Người dùng</a></li>
+			            </ul>
+			        </div>
+			        <div class="col-xs-9">
+			            <!-- Tab panes -->
+			            <div class="tab-content">
+			                <div class="tab-pane active" id="product">
+			                	<div class="col-xs-12 mgr-b-50">
+				                	<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Sản phẩm thêm mới trong "tháng này"
+				                		</h4>
+			                			<h3>{{ $count_product_thismonth }}</h3>
+				                	</div>
+				                	<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Tổng số lượng sản phẩm
+				                		</h4>
+			                			<h3>{{ $count_product_all }}</h3>
+				                	</div>
+				                </div>
+			                	<div class="col-xs-12 table-responsive">
+			                		<h4>Các sản phẩm được thêm trong tháng này</h4>
+			                		<p></p>
+			                        <table class="table table-striped datatable-index-stat-prod">
+			                            <thead>
+			                                <tr>
+			                                    <th>Tên sản phẩm</th>
+			                                    <th>Loại</th>
+			                                    <th>Giá sản phẩm</th>
+			                                    <th>Mô tả</th>
+			                                    <th>Thẻ gắn</th>
+			                                    <th>Ngày tạo</th>
+			                                </tr>
+			                            </thead>
+			                            <tbody>
+			                                @if(!$product_thismonth->isEmpty())
+			                                	@foreach($product_thismonth as $item)
+			                                		<tr>
+			                                			<td>{{ $item->name }}</td>
+			                                			<td>{{ $item->code_product }}</td>
+			                                			<td>{{ $item->price }}</td>
+			                                			<td>{{ $item->description }}</td>
+			                                			<td>{{ $item->tag }}</td>
+			                                			<td>{{ $item->created_at }}</td>
+			                                		</tr>
+			                                	@endforeach
+			                                @endif
+			                            </tbody>
+			                        </table>
+			                    </div>
+			                </div>
+			                <div class="tab-pane" id="order">
+			                	<div class="col-xs-12 mgr-b-50">
+			                		<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Lượng hóa đơn trong "tháng trước"
+				                		</h4>
+			                			<h3>{{ $count_order_lastmonth }}</h3>
+				                	</div>
+				                	<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Lượng hóa đơn trong "tuần này"
+				                		</h4>
+			                			<h3>{{ $count_order_thisweek }}</h3>
+				                	</div>
+				                	<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Lượng hóa đơn trong "tháng này"
+				                		</h4>
+			                			<h3>{{ $count_order_thismonth }}</h3>
+				                	</div>
+				                	<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Tổng số hóa đơn
+				                		</h4>
+			                			<h3>{{ $count_order_all }}</h3>
+				                	</div>
+				                </div>
+			                	<div class="col-xs-12 table-responsive">
+			                		<h4>Các hóa đơn trong tháng này</h4>
+			                        <table class="table table-striped datatable-index-stat-prod">
+			                            <thead>
+			                                <tr>
+			                                    <th>Mã hóa đơn</th>
+			                                    <th>Email khách hàng</th>
+			                                    <th>Tổng giá trị hóa đơn</th>
+			                                    <th>Trạng thái hóa đơn</th>
+			                                    <th>Ngày tạo</th>
+			                                </tr>
+			                            </thead>
+			                            <tbody>
+			                                @if(!$order_thismonth->isEmpty())
+			                                	@foreach($order_thismonth as $item)
+			                                		<tr>
+			                                			<td>{{ $item->id }}</td>
+			                                			<td>{{ $item->email }}</td>
+			                                			<td>{{ $item->total_order }}</td>
+			                                			<td>{{ $item->status }}</td>
+			                                			<td>{{ $item->created_at }}</td>
+			                                		</tr>
+			                                	@endforeach
+			                                @endif
+			                            </tbody>
+			                        </table>
+			                    </div>
+			                </div>
+			                <div class="tab-pane" id="user">
+			                	<div class="col-xs-12 mgr-b-50">
+			                		<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Lượng người dùng trong "tháng trước"
+				                		</h4>
+			                			<h3>{{ $count_user_lastmonth }}</h3>
+				                	</div>
+				                	<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Lượng người dùng trong "tuần này"
+				                		</h4>
+			                			<h3>{{ $count_user_thisweek }}</h3>
+				                	</div>
+				                	<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Lượng người dùng trong "tháng này"
+				                		</h4>
+			                			<h3>{{ $count_user_thismonth }}</h3>
+				                	</div>
+				                	<div class="col-xs-12 col-sm-6 text-center col-prod">
+				                		<h4 class="">
+				                			Tổng
+				                		</h4>
+			                			<h3>{{ $count_user_all }}</h3>
+				                	</div>
+				                </div>
+			                	<div class="col-xs-12 table-responsive">
+			                		<h4>Người dùng được thêm trong tháng này</h4>
+			                        <table class="table table-striped datatable-index-stat-prod">
+			                            <thead>
+			                                <tr>
+			                                    <th>Họ đệm</th>
+			                                    <th>Tên</th>
+			                                    <th>Email</th>
+			                                    <th>Phone</th>
+			                                    <th>Ngày tạo</th>
+			                                </tr>
+			                            </thead>
+			                            <tbody>
+			                                @if(!$user_thismonth->isEmpty())
+			                                	@foreach($user_thismonth as $item)
+			                                		<tr>
+			                                			<td>{{ $item->first_name }}</td>
+			                                			<td>{{ $item->last_name }}</td>
+			                                			<td>{{ $item->email }}</td>
+			                                			<td>{{ $item->phone }}</td>
+			                                			<td>{{ $item->created_at }}</td>
+			                                		</tr>
+			                                	@endforeach
+			                                @endif
+			                            </tbody>
+			                        </table>
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+			        <div class="clearfix"></div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection @section('myJs')
+@endsection 
+@section('myCss')
+	<style type="text/css">
+		.tabs-left, .tabs-right {
+		  border-bottom: none;
+		  padding-top: 2px;
+		}
+		.tabs-left {
+		  border-right: 1px solid #ddd;
+		}
+		.tabs-right {
+		  border-left: 1px solid #ddd;
+		}
+		.tabs-left>li, .tabs-right>li {
+		  float: none;
+		  margin-bottom: 2px;
+		}
+		.tabs-left>li {
+		  margin-right: -1px;
+		}
+		.tabs-right>li {
+		  margin-left: -1px;
+		}
+		.tabs-left>li.active>a,
+		.tabs-left>li.active>a:hover,
+		.tabs-left>li.active>a:focus {
+		  border-bottom-color: #ddd;
+		  border-right-color: transparent;
+		}
+
+		.tabs-right>li.active>a,
+		.tabs-right>li.active>a:hover,
+		.tabs-right>li.active>a:focus {
+		  border-bottom: 1px solid #ddd;
+		  border-left-color: transparent;
+		}
+		.tabs-left>li>a {
+		  border-radius: 4px 0 0 4px;
+		  margin-right: 0;
+		  display:block;
+		}
+		.tabs-right>li>a {
+		  border-radius: 0 4px 4px 0;
+		  margin-right: 0;
+		}
+		.vertical-text {
+		  margin-top:50px;
+		  border: none;
+		  position: relative;
+		}
+		.vertical-text>li {
+		  height: 20px;
+		  width: 120px;
+		  margin-bottom: 100px;
+		}
+		.vertical-text>li>a {
+		  border-bottom: 1px solid #ddd;
+		  border-right-color: transparent;
+		  text-align: center;
+		  border-radius: 4px 4px 0px 0px;
+		}
+		.vertical-text>li.active>a,
+		.vertical-text>li.active>a:hover,
+		.vertical-text>li.active>a:focus {
+		  border-bottom-color: transparent;
+		  border-right-color: #ddd;
+		  border-left-color: #ddd;
+		}
+		.vertical-text.tabs-left {
+		  left: -50px;
+		}
+		.vertical-text.tabs-right {
+		  right: -50px;
+		}
+		.vertical-text.tabs-right>li {
+		  -webkit-transform: rotate(90deg);
+		  -moz-transform: rotate(90deg);
+		  -ms-transform: rotate(90deg);
+		  -o-transform: rotate(90deg);
+		  transform: rotate(90deg);
+		}
+		.vertical-text.tabs-left>li {
+		  -webkit-transform: rotate(-90deg);
+		  -moz-transform: rotate(-90deg);
+		  -ms-transform: rotate(-90deg);
+		  -o-transform: rotate(-90deg);
+		  transform: rotate(-90deg);
+		}
+		.col-prod {
+			border: 1px solid #ddd;
+		}
+		.mgr-b-50 {
+			margin-bottom: 50px
+		}
+	</style>
+@endsection
+@section('myJs')
 <script type="text/javascript">
 jQuery(document).ready(function($) {
-    $('.datatable-index-users').DataTable({
+    $('.datatable-index-stat-prod').DataTable({
         "language": {
             "lengthMenu": "Hiển thị _MENU_ bản ghi trên 1 trang",
             "zeroRecords": "Không tìm thấy bản ghi nào",
