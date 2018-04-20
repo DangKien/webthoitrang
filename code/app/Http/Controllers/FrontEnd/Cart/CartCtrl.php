cc42252df5a91de3d3f5fa83b19016619263e4d0
namth1996 <namth1996@gmail.com>
2018-04-20 15:33:14 +0700

    THÊM GIỎ HÀNG VÀO DATABASE

--- code/app/Http/Controllers/FrontEnd/Cart/CartCtrl.php
+++ code/app/Http/Controllers/FrontEnd/Cart/CartCtrl.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\FrontEnd\Cart;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Auth, Cart, DB;
 
 
 class CartCtrl extends Controller
@@ -12,4 +14,47 @@ class CartCtrl extends Controller
     	return view('FrontEnd.content.cart.cart');
     }
 
+    public function checkout(Order $order) {
+    	if (Auth::guard('customer')->check()) {
+    		if (Cart::count() != 0){
+                if (!empty(Auth::guard('customer')->user()->address)
+                && !empty(Auth::guard('customer')->user()->phone)){
+                    DB::beginTransaction();
+                    try {
+                        $order->user_id     = Auth::guard('customer')->user()->id;
+                        $data = array();
+                        foreach (Cart::content() as $key => $value) {
+                            $product_order = 
+                                [
+                                'product'  => [
+                                    'id'        => $value->id,
+                                    'name'      => $value->name,
+                                    'url_image' => $value->options->image,
+                                    'price'     => $value->price,
+                                ],
+                                'quantity' => $value->qty ];
+                            array_push($data, $product_order);
+                        }
+                        $order->data        = json_encode($data);
+                        $order->total       = Cart::subtotal();
+                        $order->total_order = Cart::subtotal();
+                        $order->status      = 0;
+                        $order->address     = Auth::guard('customer')->user()->address;
+                        $order->save();
+                        DB::commit();
+                        Cart::destroy();
+                        return redirect()->route('home');
+                    } catch (Exception $e) {
+                        DB::rollback();
+                    }
+                } else {
+                    
+                }
+    		} else {
+    			return redirect()->back();
+    		}
+    	} else {
+    		return redirect()->route('login.frontend');
+    	}
+    }
 }
